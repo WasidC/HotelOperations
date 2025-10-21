a33d34c98adf7746e0ad78f2a4c29fb5ccb27141
wasidc <wasid.chowdhury@icloud.com>
2025-10-21 10:52:52 -0400

    Employee exercise 2

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -7,16 +7,21 @@ public class Employee {
     private double payRate;
     private double hoursWorked;
 
+    private Double timeIn; // To track when they clocked in
+
     // Constructor
+
     public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
         this.name = name;
         this.department = department;
         this.payRate = payRate;
         this.hoursWorked = hoursWorked;
+        this.timeIn = null;
     }
 
-    // Getters
+    // Existing Getters
+
     public int getEmployeeId() {
         return employeeId;
     }
@@ -38,6 +43,7 @@ public class Employee {
     }
 
     // Derived getters
+
     public double getRegularHours() {
         return Math.min(hoursWorked, 40);
     }
@@ -50,5 +56,32 @@ public class Employee {
         double regularPay = getRegularHours() * payRate;
         double overtimePay = getOvertimeHours() * payRate * 1.5;
         return regularPay + overtimePay;
+    }
+
+    // exercise 2
+
+
+    public void punchIn(double time) {
+        if (timeIn != null) {
+            System.out.println(name + " has already punched in.");
+        } else {
+            timeIn = time;
+            System.out.println(name + " punched in at " + time + ".");
+        }
+    }
+
+    public void punchOut(double time) {
+        if (timeIn == null) {
+            System.out.println(name + " must punch in before punching out.");
+        } else {
+            double hours = time - timeIn;
+            if (hours < 0) {
+                System.out.println("Invalid time: punch out cannot be before punch in.");
+            } else {
+                hoursWorked += hours;
+                System.out.println(name + " punched out at " + time + " (worked " + hours + " hours).");
+            }
+            timeIn = null; // reset for next shift
+        }
     }
 }
